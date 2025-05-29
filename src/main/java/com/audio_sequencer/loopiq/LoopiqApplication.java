d9715d7b6358fa4d809c5881f8a75072d7d5718a
GZ <gregoryzirn@gmail.com>
2025-05-30 01:15:47 +0200

    Initial commit - Configuration Spring Boot

---
+++ src/main/java/com/audio_sequencer/loopiq/LoopiqApplication.java
@@ -0,0 +1,13 @@
+package com.audio_sequencer.loopiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopiqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audio_sequencer/loopiq/LoopiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio_sequencer.loopiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
